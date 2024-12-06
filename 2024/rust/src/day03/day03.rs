13363da2d8e2abc70ac4cf748e8e0e11b213ba44
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2024-12-05 22:21:26 -0500

    day3 part 2

--- 2024/rust/src/day03/day03.rs
+++ 2024/rust/src/day03/day03.rs
@@ -16,73 +16,7 @@ fn multiply(eq: &str, re: &Regex) -> i32 {
     numbers[0] * numbers[1]
 }
 
-fn split_on_dont(line: &str) -> Option<(String, String)> {
-    println!("split_on_dont\n\n");
-    let re = Regex::new(r"don't\(\)").unwrap();
 
-    if let Some(mat) = re.find(line) {
-        let (before, after) = line.split_at(mat.start());
-        Some((
-            before.to_string(),
-            after[mat.as_str().len()..].to_string()
-        ))
-    } else {
-        None
-    }
-}
-
-fn split_on_do(line: &str) -> Option<(String, String)> { 
-    println!("split_on_dont\n\n");
-    let re = Regex::new(r"do\(\)").unwrap();
-    
-    if let Some(mat) = re.find(line) {
-        let (before, after) = line.split_at(mat.start());
-        Some((
-            before.to_string(),
-            after[mat.as_str().len()..].to_string()
-        ))
-    } else {
-        None
-    }
-}
-
-fn collect_sums (input: &str) -> i32 { 
-    println!("collect_sums\n");
-    let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
-    let nums_re = Regex::new(r"\d+").unwrap();
-    let mut sum = 0;
-    let muls = find_matches(&input, &re);
-    for mul in muls {
-        sum += multiply(&mul, &nums_re);
-    } 
-
-    sum
-}
-
-fn add_left_of_donts(input: &str) -> i32{
-    println!("============================= add_left_of_donts =========================\n\n");
-    let mut sum = 0;
-    let Some((left, right)) = split_on_dont(&input) else {
-        println!("no don'ts found\n\n");
-        return collect_sums(&input);
-    };
-    println!("left:\n\n");
-    println!("{left}\n\n");
-    println!("right:\n\n");
-    println!("{right}");
-
-    sum += collect_sums(&left);
-
-    let Some((_, remainder)) = split_on_do(&right) else {
-        println!("no do's found\n\n");
-        return sum;
-    };
-    println!("remainder:\n\n");
-    println!("{remainder}");
-    sum += add_left_of_donts(&remainder);
-
-    sum 
-}
 
 pub fn day03_part1() {
     let input = crate::get_days_input("03").unwrap();
@@ -103,13 +37,19 @@ pub fn day03_part1() {
 
 pub fn day03_part2() {
     let input = crate::get_days_input("03").unwrap();
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|(d)(o)(?:n't)?\(\)").unwrap();
     let mut sum = 0;
+    let mut enabled = true;
     for line in input {
-        println!("============================= line =========================");
         let line = line.unwrap();
-        sum += add_left_of_donts(&line);
+        for (s, [x, y]) in re.captures_iter(&line).map(|c| c.extract()) {
+            match s {
+                "do()" => enabled = true,
+                "don't()" => enabled = false,
+                _ => sum += x.parse::<i32>().unwrap() * y.parse::<i32>().unwrap() * enabled as i32,
+            }
+        }
     }
-    
 
-    println!("part 2: {sum}");
+    println!("p03 part 2: {}", sum);
 }
